635431130fbb74776a334607837069e6fc405075
maciejaroslaw <maciejaroslaw@gmail.com>
2023-09-13 11:36:15 +0200

    share link change

--- src/components/ToolBar/Share.jsx
+++ src/components/ToolBar/Share.jsx
@@ -1,20 +1,25 @@
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { showToast } from '../../store/ui-slice';
 import { useLocation } from 'preact-iso';
 
 import share from '../../assets/share.svg';
-import {updateChat} from "../../store/chats-slice.js";
+import { updateChat } from "../../store/chats-slice.js";
 
 export function Share() {
   const dispatch = useDispatch();
-  const location = useLocation();
   const currentChat = useSelector(state => state.chats.currentChat);
 
   function callShowToast() {
     navigator.clipboard.writeText(window.location.href);
     let sharedInfo = !currentChat.share ? 'shared' : 'unshared';
+
+    if (currentChat.share) {
+      dispatch(showToast({ content: `Sharing chat by link disabled` }))
+    } else {
+      dispatch(showToast({ content: `Link copied to clipboard` }))
+    }
+
     dispatch(updateChat({ uuid: currentChat.uuid, share: !currentChat.share }));
-    dispatch(showToast({ content: `Succesfully ${sharedInfo}` }))
   }
 
   return (
