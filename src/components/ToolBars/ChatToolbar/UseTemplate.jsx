c9eb3e29d2689584a0a8986d59155909c2423c7f
maciejaroslaw <maciejaroslaw@gmail.com>
2023-10-02 14:33:06 +0200

    useTemp Style

--- src/components/ToolBars/ChatToolbar/UseTemplate.jsx
+++ src/components/ToolBars/ChatToolbar/UseTemplate.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { signal } from '@preact/signals';
+import { useEffect, useRef } from 'preact/hooks';
 
 import braces from '../../../assets/braces.svg';
 
@@ -9,14 +10,35 @@ function toggleVisible() {
   isVisible.value = !isVisible.value
 }
 
+function outsideClickHanlder(ref) {
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        isVisible.value = false;
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+    }
+  }, [ref])
+}
+
 
 export function UseTemplate(props) {
   const templates = useSelector(state => state.templates.templates);
 
+  const useTempRef = useRef(null);
+  outsideClickHanlder(useTempRef);
+
+
   return (
-    <div className={'relative'}>
-      <div onClick={toggleVisible} className={'border border-b-0 rounded-t border-[#DBDBDB] w-8 h-8 flex items-center justify-center'}>
-        <img src={braces} alt="" />
+    <div ref={useTempRef} className={'relative'}>
+      <div onClick={toggleVisible} className={'border p-1 border-b-0 cursor-pointer rounded-t border-[#DBDBDB] w-8 h-8 flex items-center justify-center '}>
+        <div className={'p-1 hover:bg-[#F2F2F2] ' + (isVisible.value ? 'bg-[#F2F2F2]' : '')}>
+          <img src={braces} alt="" />
+        </div>
       </div>
       <div className={"absolute border rounded left-8 w-5/12 bottom-12 p-2 transform bg-[#202020] text-white " + (isVisible.value ? '' : 'hidden')}>
         {templates.map(template => (
