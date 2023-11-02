673456d0348d248ed45379f80a62eff74cbf8973
maciejaroslaw <maciejaroslaw@gmail.com>
2023-11-02 12:35:14 +0100

    PA-79 layout fix

--- src/components/SideBar/Footer/Options.jsx
+++ src/components/SideBar/Footer/Options.jsx
@@ -62,7 +62,7 @@ export function Options(props) {
           <path fill-rule="evenodd" clip-rule="evenodd" fill="currentColor" d="M2 6C3.10457 6 4 6.89543 4 8C4 9.10457 3.10457 10 2 10C0.89543 10 0 9.10457 0 8C0 6.89543 0.89543 6 2 6ZM8 6C9.10457 6 10 6.89543 10 8C10 9.10457 9.10457 10 8 10C6.89543 10 6 9.10457 6 8C6 6.89543 6.89543 6 8 6ZM16 8C16 6.89543 15.1046 6 14 6C12.8954 6 12 6.89543 12 8C12 9.10457 12.8954 10 14 10C15.1046 10 16 9.10457 16 8Z" />
         </svg>
       </div>
-      <div className={"absolute border border-[#595959] rounded min-w-[160px] -top-[8.5rem] -left-[8rem] bg-[#0F0F0F] " + (showOptions.value ? '' : 'hidden')}>
+      <div className={"absolute border border-[#595959] rounded min-w-[160px] -top-[5.5rem] -left-[8rem] bg-[#0F0F0F] " + (showOptions.value ? '' : 'hidden')}>
         <div className="text-sm leading-6">
           <div className={"cursor-pointer flex p-2 hover:bg-[#595959]"}>
             <img className="w-4" src={darkmode} alt="" /> <div className="ml-2">Toggle Theme</div>
