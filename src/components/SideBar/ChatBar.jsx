bf669ef81e3395944a913ec094fb2e09410382a4
maciejaroslaw <maciejaroslaw@gmail.com>
2023-11-07 16:22:46 +0100

    PA-92

--- src/components/SideBar/ChatBar.jsx
+++ src/components/SideBar/ChatBar.jsx
@@ -20,11 +20,37 @@ export const ChatBar = props => {
     return props.ChatData.uuid === currentChat.uuid
   }
 
+  function EditedAt() {
+    const updatedAt = new Date(props.ChatData.updated_at ? props.ChatData.updated_at : props.ChatData.created_at);
+    const today = new Date();
+
+    // const diffrence = Math.floor((today.getTime() - updatedAt.getTime()) / (1000 * 3600 * 24));
+    var hours = Math.floor(Math.abs(today - updatedAt) / 36e5);
+    // var hours = Math.floor(12 * 24);
+
+    // if (hours === 0) {
+    // 	return 'Just now';
+    // }
+
+    if (hours < 24) {
+      return `${hours} ${hours > 1 ? 'hours' : 'hour'} ago`
+    } else if (hours >= 24 && hours < 7 * 24) {
+      let days = Math.floor(hours / 24);
+      return `${days} ${days > 1 ? 'days' : 'day'} ago`
+    } else if (hours >= 7 * 24) {
+      let weeks = Math.floor((hours / 24) / 7)
+      return `${weeks} ${weeks > 1 ? 'weeks' : 'week'} ago`
+    }
+  }
+
   return (
     <div onClick={callSelectChat} className={"flex items-center py-2 px-2 rounded cursor-pointer " + (isSelected() ? 'bg-[#595959]' : 'hover:bg-[#0F0F0F]')}>
       <img className={"w-4"} src={props.ChatData.visibility === 'just_me' ? meChat : chatIcon} alt="" />
       <div title={props.ChatData.name} className="font-bold text-sm leading-6 ml-2 truncate">
         {props.ChatData.name}
+      </div>
+      <div className={'ml-auto text-xs shrink-0 ' + (isSelected() ? 'text-[#DBDBDB]' : 'text-[#747474]')}>
+        {EditedAt()}
       </div>
     </div>
   )
