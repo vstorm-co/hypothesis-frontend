e0165098de68bdbc74fa94e9c6f278301e3b7d18
maciejaroslaw <maciejaroslaw@gmail.com>
2023-11-10 14:42:17 +0100

    PA-110

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from 'react-redux';
 import ReactMarkdown from 'react-markdown';
 import rehypePrism from '@mapbox/rehype-prism';
-import { SaveAsTemplate } from './ToolBars/ChatToolbar/SaveAsTemplate';
+import SaveAsTemplate from './ToolBars/ChatToolbar/SaveAsTemplate';
 
 import papaya from '../assets/images/papaya.png';
 import CopyAs from './ToolBars/ChatToolbar/CopyAs';
@@ -10,17 +10,18 @@ import { useState } from 'preact/hooks';
 export function Message(props) {
   const dispatch = useDispatch();
 
+  const [showSaveAs, setShowSaveAs] = useState(false);
   const [showCopyAs, setShowCopyAs] = useState(false);
 
   if (props.Message.created_by === 'user') {
     return (
-      <div className="flex items-start my-4 group">
+      <div onMouseLeave={() => setShowSaveAs(false)} className="flex items-start my-4 group">
         <div className="bg-[#F2F2F2] rounded-lg flex items-start p-2 pr-3 max-w-3xl">
           <img src={props.Message.sender_picture} className="w-8 h-8 border border-[#DBDBDB] rounded-full" />
           <div className="ml-4 self-center text-[#202020] text-sm" title={props.Message.content} dangerouslySetInnerHTML={{ __html: props.Message.content_html ? props.Message.content_html : props.Message.content }}></div>
         </div>
         <div className={'ml-auto hidden group-hover:flex items-center shrink-0'}>
-          <SaveAsTemplate msg={props.Message} />
+          <SaveAsTemplate toggleShowSaveAs={tgl => setShowSaveAs(tgl)} showSaveAs={showSaveAs} msg={props.Message} />
         </div>
       </div>
     )
--- src/components/ToolBars/ChatToolbar/SaveAsTemplate.jsx
+++ src/components/ToolBars/ChatToolbar/SaveAsTemplate.jsx
@@ -1,45 +1,68 @@
 import { signal } from '@preact/signals';
-import { useDispatch } from 'react-redux';
 import { createTemplate } from '../../../store/templates-slice';
 
 import braces from '../../../assets/braces.svg';
-import { useState } from 'preact/hooks';
-import { uiActions } from '../../../store/ui-slice';
+import { Component, createRef } from 'preact';
+import { connect } from 'react-redux';
 
-const showSaveAs = signal(false);
-function toggleShowSaveAs() {
-  showSaveAs.value = !showSaveAs.value;
-}
-
-export function SaveAsTemplate(props) {
-  const dispatch = useDispatch();
-  const [templateTitle, setTemplateTitle] = useState();
-
-  function callCreateTemplate(e) {
-    if (e.target.value.length > 1) {
-      dispatch(createTemplate({ name: e.target.value, content: props.msg.content, content_html: props.msg.content_html }));
+class SaveAsTemplate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      templateTitle: "",
     }
-    toggleShowSaveAs();
+    this.SaveAsTemplateRef = createRef();
+    this.handleClickOutside = this.handleClickOutside.bind(this);
   }
 
-  return (
-    <div className={'relative'}>
-      <div onClick={toggleShowSaveAs} className={"p-1 border border-[#DBDBDB] rounded-r cursor-pointer"}>
-        <div className={"p-1 hover:bg-[#F2F2F2] " + (showSaveAs.value ? 'bg-[#F2F2F2]' : '')}>
-          <img className="w-4" src={braces} alt="edit" />
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  handleClickOutside(e) {
+    if (this.SaveAsTemplateRef && !this.SaveAsTemplateRef.current.contains(e.target)) {
+      this.props.toggleShowSaveAs(false);
+    }
+  }
+
+  toggleShowSaveAs = () => {
+    this.props.toggleShowSaveAs(!this.props.showSaveAs);
+  }
+
+  callCreateTemplate = (e) => {
+    if (e.target.value.length > 1) {
+      this.props.dispatch(createTemplate({ name: e.target.value, content: this.props.msg.content, content_html: this.props.msg.content_html }));
+    }
+    this.props.toggleShowSaveAs(!this.props.showSaveAs);
+  }
+
+
+  render() {
+    return (
+      <div ref={this.SaveAsTemplateRef} className={'relative mt-2'}>
+        <div onClick={this.toggleShowSaveAs} className={"p-1 border border-[#DBDBDB] rounded-r cursor-pointer"}>
+          <div className={"p-1 hover:bg-[#F2F2F2] " + (this.props.showSaveAs ? 'bg-[#F2F2F2]' : '')}>
+            <img className="w-4" src={braces} alt="edit" />
+          </div>
+        </div>
+        <div className={"absolute z-50 border rounded right-0 top-10 bg-white " + (this.props.showSaveAs ? '' : 'hidden')}>
+          <div className="border-b p-2">
+            <div className="text-xs font-bold text-[#747474] mb-1">
+              Title
+            </div>
+            <input onChangeCapture={(event) => this.callCreateTemplate(event)} type="text" className="bg-[#F2F2F2] border border-[#DBDBDB] rounded focus:outline-none p-2" />
+            <div className={'text-[10px] mt-0.5 text-right text-[#747474]'}>
+              press 'Enter' to confirm
+            </div>
+          </div>
         </div>
       </div>
-      <div className={"absolute z-50 border rounded right-0 top-10 bg-white " + (showSaveAs.value ? '' : 'hidden')}>
-        <div className="border-b p-2">
-          <div className="text-xs font-bold text-[#747474] mb-1">
-            Title
-          </div>
-          <input onChangeCapture={(event) => callCreateTemplate(event)} type="text" className="bg-[#F2F2F2] border border-[#DBDBDB] rounded focus:outline-none p-2" />
-          <div className={'text-[10px] mt-0.5 text-right text-[#747474]'}>
-            press 'Enter' to confirm
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default connect(null)(SaveAsTemplate)
