07c2c8fd27fe70f8c4f682667680151a19ba9b4b
maciejaroslaw <maciejaroslaw@gmail.com>
2023-11-16 10:31:50 +0100

    PA-109

--- src/components/ToolBars/TemplateToolbar/TemplateToolBar.jsx
+++ src/components/ToolBars/TemplateToolbar/TemplateToolBar.jsx
@@ -1,9 +1,20 @@
+import { useSelector } from 'react-redux';
 import { Edit } from './Edit';
 
 export function TemplateToolBar(props) {
-  return (
-    <div className="flex">
-      <Edit callEditTemplate={props.callEditTemplate} />
-    </div>
-  )
+  const user = useSelector(state => state.user.currentUser);
+  const currentTemplate = useSelector(state => state.templates.currentTemplate);
+
+  if (user.user_id === currentTemplate.user_id) {
+    return (
+      <div className="flex">
+        <Edit callEditTemplate={props.callEditTemplate} />
+      </div>
+    )
+  } else {
+    return (
+      <div className="flex">
+      </div>
+    )
+  }
 }
