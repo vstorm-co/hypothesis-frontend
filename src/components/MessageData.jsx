86a4e7b3624fe070190af588ad8b28a84a3d25bb
maciejaroslaw <maciejaroslaw@gmail.com>
2024-04-11 12:18:33 +0200

    add GPT version

--- src/components/MessageData.jsx
+++ src/components/MessageData.jsx
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
+
 export function MessageData(props) {
     function timestamp() {
         let time = new Date(props.Message.created_at).toUTCString()
         return time;
     }
 
+    const currentChat = useSelector(state => state.chats.currentChat)
+
     return (
         <div style={{ ...props.Position }} className={'fixed py-2 px-3 bg-white w-[240px] z-50 rounded border text-xs ' + (props.Visible ? 'block' : 'hidden')}>
-            <div className={''}><span className={'font-bold text-[#747474]'}>Model:</span> GPT-4</div>
+            <div className={''}><span className={'font-bold text-[#747474]'}>Model:</span> {currentChat?.model_name}</div>
             <div title={timestamp()} className={'mt-0.5'}><span className={'font-bold text-[#747474]'}>Timestamp:</span> {timestamp()}</div>
             <div className={'mt-2'}>
                 <span className={'font-bold text-[#747474]'}>Tokens:</span>
