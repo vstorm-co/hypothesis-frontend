0d81a6b39809c0e558981a2fe8954a661a8d806b
maciejaroslaw <maciejaroslaw@gmail.com>
2024-08-29 15:36:30 +0200

    fix:PA-339

--- src/index.jsx
+++ src/index.jsx
@@ -21,7 +21,7 @@ import { SetUp } from './pages/SetUp.jsx';
 import { Chat } from './pages/Chat.jsx';
 
 import { getUserOrganizationsData } from "./store/user-slice";
-import { getOrganizationData } from './store/organizations-slice';
+import { fetchModels } from './store/ui-slice';
 import { getTemplatesData } from "./store/templates-slice";
 import { getChatsData, chatsActions, selectChat } from "./store/chats-slice";
 import { fetchAvailableProviders, fetchModels, uiActions } from './store/ui-slice';
@@ -79,7 +79,7 @@ export function App() {
 					store.dispatch(uiActions.toggleFileUpdating(false));
 				}
 			} else if (json_data.type === 'user-model-changed') {
-				store.dispatch(getOrganizationData(state.user.currentUser.organization_uuid));
+				store.dispatch(fetchModels());
 			}
 		}
 	});
